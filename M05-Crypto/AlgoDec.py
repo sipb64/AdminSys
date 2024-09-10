167268250174f81512a0d58e5aa0b2b5d69d5b37
michael <sipb64@gmail.com>
2024-09-10 08:51:39 +0200

    corralgcryp

--- M05-Crypto/AlgoDec.py
+++ M05-Crypto/AlgoDec.py
@@ -62,3 +62,43 @@ print(f"Mess : {decrypt}")
 #             break
 #     except Exception as e:
 #         continue
+
+# Correction 
+
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding
+from cryptography.hazmat.backends import default_backend
+
+def des_encrypt(key, plaintext):
+    cipher = Cipher(algorithms.TripleDES(key), modes.ECB(), backend=default_backend())
+    encryptor = cipher.encryptor()
+    padder = padding.PKCS7(64).padder()
+    padded_data = padder.update(plaintext) + padder.finalize()
+    ct = encryptor.update(padded_data) + encryptor.finalize()
+    return ct
+
+def des_decrypt(key, ciphertext):
+    cipher = Cipher(algorithms.TripleDES(key), modes.ECB(), backend=default_backend())
+    decryptor = cipher.decryptor()
+    pt = decryptor.update(ciphertext) + decryptor.finalize()
+    unpadder = padding.PKCS7(64).unpadder()
+    unpadded_data = unpadder.update(pt) + unpadder.finalize()
+    return unpadded_data
+
+key = None
+CypherText = b'\xd72U\xc03.\xda\x99Q\xb5\x020\xc4\xb8\x16\xc6\xfa-\xb9U+\xda\\\x126L\xf3~\xbd8\x12q\x02?\x80\xeaVI\xa9\xe1'
+chars = "abcdefghijklmnopqrstuvwxyz"
+
+for g in chars:
+    for h in chars:
+        for i in chars:
+            for j in chars:                             
+                key = '12345678bien'+ g + h + i + j  
+                #key = "12345678bienjoue"
+                try:
+                    ClearText = des_decrypt(key.encode(), CypherText)
+                    print(ClearText.decode("utf-8"))
+                    print(key)           
+                except Exception as err:
+                    #print(type(err).__name__)
+                    pass
